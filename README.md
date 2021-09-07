bd5af8daf997420acae809234283ad43500ebb5f
ariadnagalindom <ariadnagalindom@gmail.com>
2021-09-07 13:10:22 -0500

    README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # MyST
  Microestructuras y Sistemas de Traiding 
+
+ ESTRUCTURA DEL PROYECTO: 
+
+1. files: una carpeta donde se encuentran los archivos a utilizar 
+2. venv: carpeta donde se encuentra el ambiente virtual del proyecto 
+3.  requirements.txt: archivo .txt donde se enlistan los requerimientos de librerias que necesita el ambiente (pip freeze > requirements.
+txt)
+4. main.py: script donde se mandan llamar funciones y se presentan codigos para el acomodo de datos, aquí se mandan llamar functions.py
+ y visualizations.py, haciendo uso de lo almacenado en data.py
+5. data.py: script donde se guardan datos de utilidad en diferentes scripts 
+6. functions.py: script donde declaramos funciones de medianas a complejas que se usaran en main.py 
+7. visualizations.py: script donde se dejan todas las funciones desarrolladas para visualizar datos como tablas, gráficas, textos impresos, etc. 
+8. notebook.ipynb: cuaderno de jupyter donde se explica todo el proyecto, va de la mano con main.py con la única diferencia de que en el notebook se explica detalladamente en entradas de texto. A su vez, debe guardarse como .html 
+9. README.md: archivo introductorio del proyecto
+10. .gitignore: tipo .txt donde se almacenan archivos a ignorar en GitHub
+
+MÁS SOBRE EL PROYECTO: 
+Dentro de la materia de Microestructuras y Sistemas de Traiding para Otoño 2021 se crea simula una inversión de un millón de pesos 
+con dos estrategias diferentes; activa y pasiva. 
---
+++ ariadna.py
@@ -0,0 +1,80 @@
+# Descargarmos librerias 
+from numpy.lib.function_base import append
+from numpy.testing._private.utils import print_assert_equal
+import pandas as pd
+import numpy as np
+from pandas.core.frame import DataFrame
+import yfinance as yf
+from datetime import date, datetime
+from os import path
+import os 
+import xlrd
+import sys 
+import glob
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+from datetime import timedelta
+
+cedir = os.path.dirname(os.path.abspath(sys.argv[0]))
+print(cedir)
+
+# Definir capital, comision y tasa libre de riesgo.
+capital = 1000000
+comision = 0.00125
+rf = 0.0429
+ 
+# Obtenemos un DataFrame con las fechas y las ordenamos.
+fechas = []
+
+for files in glob.glob(cedir+ '\\MyST\\files\\*.csv'):
+    fechas.append(files[-12:-8] + files[-8:-6] + files[-6:-4])
+print(fechas)
+
+
+fcd = pd.DataFrame()
+
+fcd['Date'] = fechas 
+
+dates = list(fcd['Date'])
+print(dates)
+print(len(dates))
+
+g = []
+i = 0
+
+while i <= 40:
+    fechas = pd.to_datetime(dates[i], format='%Y/%m/%d')
+    g.append(fechas)
+    i = i + 1
+
+a = DataFrame()
+a['Date'] = g
+a = a.sort_values(['Date'])
+a = a.reset_index()
+print(a['Date'])
+
+# Obtenemos los tickers y los pesos.
+data = pd.read_csv(cedir + '\\MyST\\files\\NAFTRAC_20180131.csv', skiprows=2)
+
+ticker  = list(data.T.iloc[0])
+ticker = pd.DataFrame(ticker)
+
+#corregimos tickers
+ticker = ticker.replace('\*','',regex=True).astype(str)
+ticker = ticker.replace('LIVEPOLC.1', 'LIVEPOLC-1')
+ticker = ticker.replace('MEXCHEM', 'ORBIA')
+ticker = ticker.replace('GFREGIOO', 'RA')
+ticker = ticker[0] + '.MX'
+ticker2 = list(ticker[0:])
+
+print(ticker2)
+weight  = data.T.iloc[3]
+
+print(len(a['Date']))
+
+# Descargamos precios 
+start = a['Date'][0]
+data_prices = yf.download(tickers=ticker2, start=start, end=a['Date'].iloc[-1], progress=False)
+price = data_prices['Close']
+print(price)
+
